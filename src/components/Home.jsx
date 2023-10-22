500eeaf155d947418e4e422075e3ffbfcab27a63
Dipen Khadka <dipenkhadka191@gmail.com>
2023-10-22 16:50:36 +0545

    Fetch Error Msg Added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
   const [product, setproduct] = useState([]);
   const { addToCart } = useContext(CartContext);
   const [loading, setloading] = useState(true);
+  const [error, seterror] = useState();
 
   const fetchproducts = async () => {
     try {
@@ -15,16 +16,42 @@ const Home = () => {
       let data = await response.json();
       setproduct(data);
     } catch (err) {
-      console.log(err);
+      seterror(err);
     }
   };
 
   useEffect(() => {
     fetchproducts();
     setTimeout(() => {
-      setloading(false);
+       setloading(false);
     }, 1200);
   }, []);
+
+  if (error != null)
+    return (
+      <div role="alert" class="rounded border-s-4 border-red-500 bg-red-50 p-4">
+        <div class="flex items-center gap-2 text-red-800">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="h-5 w-5"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+              clip-rule="evenodd"
+            />
+          </svg>
+
+          <strong class="block font-medium"> Something went wrong </strong>
+        </div>
+
+        <p class="mt-2 text-sm text-red-700">
+          Error While Fetching Products Data
+        </p>
+      </div>
+    );
 
   return (
     <>
@@ -33,7 +60,7 @@ const Home = () => {
           <div className="flex justify-center flex-wrap gap-4 ">
             {Array.from({ length: 20 }).map((_, index) => (
               <div className="" key={index}>
-                <div >
+                <div>
                   <Skeleton height={300} width={320} />
                 </div>
                 <div>
